fe65b1375718ebee7b7ef93415f9869a1c921754
saintflores <mario@mariosbarrio.com>
2012-02-13 21:28:37 -0600

    Removed comments

--- AgendaFormatter.rb
+++ AgendaFormatter.rb
@@ -5,8 +5,6 @@ class AgendaFormatter
   
   def initialize(config, roster)
     @roster = roster
-    #puts @roster['PresidingOfficer']
-    #puts @roster.inspect
     @configFile = ParseConfig.new(config)
     self.getGroups
   end
@@ -19,14 +17,11 @@ class AgendaFormatter
   
   def getValue(value)
     value = self.parseValue(value)
-    #puts value
     return value
   end
   
   def parseValue(value)
     temp = value.split('|')
-    #puts @roster['PresidingOfficer']
-    #puts temp[1]
     
     if @roster.include?(temp[1]) then
       temp[1] = @roster[temp[1]]
@@ -39,11 +34,7 @@ class AgendaFormatter
   def getGroups
     @groups = Hash.new
     @configFile.get_groups.each { |key| @groups[key] = self.getSectionItems(key) }
-    #@groups.each_pair { |key, value| puts key, value }
     return @groups
   end
   
 end
-
-
-#my_config = AgendaFormatter.new('/Users/saintflores/Temp/Workspace/AgendaCreator/src/template.config')
