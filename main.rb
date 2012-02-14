d7f48d633d97a01168424b2c50844a39bf9c76eb
saintflores <mario@mariosbarrio.com>
2012-02-13 21:26:51 -0600

    Removed comments from main.rb

--- main.rb
+++ main.rb
@@ -8,12 +8,7 @@ require 'PDFCreator'
 roster = AgendaConfig.new('/Users/saintflores/Temp/Workspace/AgendaCreator/src/roster.config')
 my_roster = RosterData.new(roster)
 my_roster = my_roster.get_current_assignees
-#puts my_roster.inspect
-#my_roster.each_pair { |key, value| puts key, value }
-#puts my_roster.inspect
-#my_roster.show_current_assignees
 template = AgendaFormatter.new('/Users/saintflores/Temp/Workspace/AgendaCreator/src/template.config', my_roster)
 template = template.getGroups
-#puts template['Header']
 pdf = PDFCreator.new(template)
 pdf.create
